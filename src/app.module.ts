d24dad6e0d269bfebe783225e8f3fbb35702cda3
Isadora <is.isadora.rocha@gmail.com>
2024-08-07 15:18:40 -0300

    nest g resource user (STRU_CRUD)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { UserModule } from './user/user.module';
 
 // O módulo raiz do aplicativo.
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
